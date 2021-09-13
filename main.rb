573c99e4a097286a84916648049e0db87a9bd866
Ellie Nam <ellienjuly@gmail.com>
2021-09-13 15:58:03 -0400

    End the game when a player lost all their lives

--- main.rb
+++ main.rb
@@ -7,17 +7,21 @@ require './guess'
 p1 = Player.new("Player 1", 3, "What does 3 plus 5 equal?")
 p2 = Player.new("Player 2", 3, "What does 1 plus 2 equal?")
 
-puts p1.question
 
-a = Guess.new(gets.chomp)
+while (p1.lives != 0 && p2.lives != 0)
+  puts p1.question
+  a = Guess.new(gets.chomp)
 
-if a.guess.to_i == 8
-  puts "YES! You are correct."
+  if a.guess.to_i == 8
+    puts "YES! You are correct."
+  else
+    p2.lives = p2.lives.to_i - 1
+    puts "#{p1.player}: Seriously? No!"
+  end
   puts "P1: #{p1.lives}/3 vs P2: #{p2.lives}/3"
   puts "----- NEW TURN -----"
-else
-  p2.lives = p2.lives.to_i - 1
-  puts "#{p1.player}: Seriously? No!"
-  puts "P1: #{p1.lives}/3 vs P2: #{p2.lives}/3"
-  puts "----- NEW TURN -----"
+
 end
+
+puts "-----GAME OVER-----"
+puts "Good bye!"
